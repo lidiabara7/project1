0ce78ce582e4e143d6c44726576310a2552b802b
kevdoan <kevdoan91@gmail.com>
2019-06-27 13:15:15 -0700

    some pseudocode!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
+//document click submit button
+//javascript for randomizing background wallpaper
+//set up firebase.. eventually. store searches into database and return as trending searches.
+
+
 function omdb() {
 
-    //var settings = {}
-    var genre = "28";
-    //var queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy"
-    //var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=cc7424fa466286ce5e0ea7956a1183ba&id=28"
-
+    var genre = "28"; //change this to jquery to attach to dropdown list
     var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=cc7424fa466286ce5e0ea7956a1183ba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=" + genre;
 
 
@@ -13,13 +14,15 @@ function omdb() {
         method: "GET",
     }).then(function (response) {
         console.log(response);
+        //obtain image and movie title, maybe a short summary?
+        //append to carousel
     });
 
 }
 
 function food() {
 
-    var search = "chicken breast";
+    var search = "chicken breast"; //jquery attach to search bar
     var queryURL = "https://www.food2fork.com/api/search?key=ee951658fe4f3d33d2e07163d207798f&q="+search;
 
     $.ajax({
@@ -27,9 +30,13 @@ function food() {
         method: "GET",
     }).then(function (response) {
         console.log(JSON.parse(response));
+        //obtain image and link to recipe
+        //append to carousel
     });
 
 }
+
+
 
 food();
 omdb();
