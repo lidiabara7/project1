32970ffeb0ee8e803d961b533a1ff43683e40b00
kevdoan <kevdoan91@gmail.com>
2019-07-02 13:50:51 -0700

    cleaned up, final product!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-// store firebase data here
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyB2qEO1v5-ETBy30qeexa0lJBm4i3DlV5A",
@@ -14,88 +13,69 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-//new todo: 
-/*
-push user input to firebase as an array and then retrieve child for appending images, instead of appending directly from user input. similar to train homework. also will help with creating a trending search. work on this tomorrow during class.
-
-... or should we handle both separately? have the searches push into firebase but also search the API right away, instead of waiting for firebase to return the child_added.
-*/
-
-var wallpapers = [
-    "assets/images/curtain-2757815_1920.png",
-    "assets/images/film-1668918_1920.jpg",
-    "assets/images/film-3057394_1920.jpg",
-    "assets/images/theater-105574_1920.jpg",
-    "assets/images/theater-1713816_1920.jpg",
-    "assets/images/theater-4023278_1920.jpg"
-];
-
 $(document).ready(function () {
-    //$('.carousel').carousel();
     $("select").formSelect();
+
+    var wallpapers = [
+        "assets/images/curtain-2757815_1920.png",
+        "assets/images/film-1668918_1920.jpg",
+        "assets/images/film-3057394_1920.jpg",
+        "assets/images/theater-105574_1920.jpg",
+        "assets/images/theater-1713816_1920.jpg",
+        "assets/images/theater-4023278_1920.jpg"
+    ];
+
 
     var randomWallpaper = Math.floor(Math.random() * wallpapers.length);
     var bgImg = "url(" + wallpapers[randomWallpaper] + ")";
+
     $("body").css(
         {
-            "background" : "linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))," + bgImg,
-            "background-attachment" : "fixed",
-            "height" : "100%"
+            "background": "linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))," + bgImg,
+            "background-attachment": "fixed",
+            "height": "100%"
         }
     );
 
+    $("button").on("click", event => {
 
+        event.preventDefault();
 
-});
+        var food_search = $("#food-input").val().trim();
+        var genre = $("#select-genre").val();
+        var genre_name = $("option:selected").text();
 
-$("button").on("click", event => {
+        //will now only run ombd_call if food_call works
+        food_call(food_search).then(result => {
+            if (result) {
+                var trending = {
+                    food: food_search,
+                    genre: genre_name
+                };
+                omdb_call(genre);
+                $("#main-container").show();
+                database.ref().push(trending);
+            }
+        });
 
-    event.preventDefault();
+        if (DEBUG) { console.table('app.js', food_search, genre, trending); }
 
-    var food_search = $("#food-input").val().trim();
-    var genre = $("#select-genre").val();
-    var genre_name = $("option:selected").text();
-
-    //will now only run ombd_call if food_call works
-    food_call(food_search).then(result => {
-        if (result) {
-            var trending = {
-                food: food_search,
-                genre: genre_name
-            };
-            omdb_call(genre);
-            $("#main-container").show();
-            database.ref().push(trending);
-
-        }
     });
 
-    //if food search is invalid, i don't want to start omdb_call. how can i implement this..?
-    //so.. if (response !== 0) then -> omdb_call(genre)?
+    database.ref().on("child_added", snapshot => {
 
+        if (DEBUG) { console.table(snapshot.food, snapshot.genre); }
 
+        //trending variables
+        var food = snapshot.val().food;
+        var genre = snapshot.val().genre;
 
+        //create p element to append to trending div
+        var trending = $("<p> #" + food + " #" + genre + "</p>");
 
-    if (DEBUG) {
-        console.table('app.js', food_search, genre, trending);
-    }
+        $("#trending").prepend(trending);
 
-
-
+    });
 });
 
-database.ref().on("child_added", snapshot => {
 
-    if (DEBUG) {
-        console.table(snapshot.food, snapshot.genre);
-    }
-    //trending variables
-    var food = snapshot.val().food;
-    var genre = snapshot.val().genre;
-
-    //create p element to append to trending div
-    var trending = $("<p> #" + food + " #" + genre + "</p>");
-    // testing, it works! altho its more of a "last search" than a trending, but we will work on that later 
-    $("#trending").prepend(trending); //<-- i have this showing up just so we can visualize it right now. will append it to a proper div tomorrow
-
-});
